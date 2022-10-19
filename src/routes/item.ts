f29ca8ccce022c69e074c810e685f6e82ce534e7
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-19 18:51:45 -0400

    add first middleware

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,13 +1,13 @@
 import { Request, Response, Router } from "express";
 import { getItem, getItems, postItem, updateItem, deleteItem } from "../controllers/item";
-
+import { logMiddleware } from "../middleware/log";
 const router = Router();
 /*
  *
  *http://localhost:3000/item [GET]
  */
 
-router.get("/:id", getItem);
+router.get("/:id", logMiddleware, getItem);
 router.get("/", getItems);
 router.post("/", postItem);
 router.put("/:id", updateItem);
