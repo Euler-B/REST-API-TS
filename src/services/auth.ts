2ffe648890077c2c9e023224b846f5fa91cd8897
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-20 16:51:33 -0400

    add authentication with JWT

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,8 @@
 import { User } from '../interfaces/user.interface';
+import { Auth } from '../interfaces/auth.interface';
 import UserModel from '../models/user';
-import { encrypt } from '../utils/bcrypt.handle';
+import { encrypt, verified } from '../utils/bcrypt.handle';
+import { generateToken } from '../utils/jwt.handle';
 
 const registerNewUser = async ({email, password, name}: User) => {
     const checkIs = await UserModel.findOne({ email });
@@ -11,8 +13,22 @@ const registerNewUser = async ({email, password, name}: User) => {
     return registerNewUser;
 };
 
-const loginUser = async () => {
+const loginUser = async ({email, password}: Auth) => {
+    const checkIs = await UserModel.findOne({ email });
+    if (!checkIs) return "Usuario no encontrado";
 
+    const passwordHash = checkIs.password;
+    const isCorrect = await verified(password, passwordHash);
+
+    if (!isCorrect) return "Error de autenticación";
+
+    const token = generateToken(checkIs.email);
+    const data = {
+        token,
+        user: checkIs,
+    };
+
+    return data;
 };  
 
 export { registerNewUser, loginUser };
